e8c5a4c7f6702ff0671d36e459e78ced08e21cd3
TruthTheDeveloper <henrytruthidongesit@gmail.com>
2022-05-25 07:49:39 -0700

    fixed the responsive issue

--- src/containers/LandingPage/components/Trends.js
+++ src/containers/LandingPage/components/Trends.js
@@ -9,67 +9,67 @@ const Trends = () => {
                 <p className="lg:text-2xl py-5 text-xl">We are committed to providing a Quality eyewear and a safe shopping</p>
             </div>
             <Carousel autoPlay={true} infiniteLoop={true}>
-                <div className="flex flex-col md:flex-row md:mx-12 lg:mx-24">
+                <div className="flex md:flex-row md:mx-12 lg:mx-24">
                     <div className="text-center">
                         <img src="https://eyeframeng.com/wp-content/uploads/2020/12/Theo-WD9511.jpg" />
                         <p>Eyeglasses Theo WD9511</p>
                         <p className="text-orange-600 font-semibold py-2 text-xl">#5000</p>
                     </div>
-                    <div className="text-center">
+                    <div className="text-center hidden md:block">
                         <img src="https://eyeframeng.com/wp-content/uploads/2021/01/Mitch.jpg" />
                         <p>Eyeglasses Mitch</p>
                         <p className="text-orange-600 font-semibold py-2 text-xl">#15000</p>
                     </div>
-                    <div className="text-center">
+                    <div className="text-center hidden md:block">
                         <img src="https://eyeframeng.com/wp-content/uploads/2021/01/Zabel-C4.jpg" />
                         <p>Eyeglasses Zabel-C4</p>
                         <p className="text-orange-600 font-semibold py-2 text-xl">#10000</p>
                     </div>
-                    <div className="text-center">
+                    <div className="text-center hidden md:block">
                         <img src="https://eyeframeng.com/wp-content/uploads/2020/12/Eyeframeng-18.jpg" />
                         <p>Eyeglasses Eyeframeng-18</p>
                         <p className="text-orange-600 font-semibold py-2 text-xl">#20000</p>
                     </div>
                 </div>
-                <div className="flex flex-col md:flex-row md:mx-12 lg:mx-24">
+                <div className="flex  md:flex-row md:mx-12 lg:mx-24">
                     <div className="text-center">
                         <img src="https://eyeframeng.com/wp-content/uploads/2020/12/Theo-WD9511.jpg" />
                         <p>Eyeglasses Theo WD9511</p>
                         <p className="text-orange-600 font-semibold py-2 text-xl">#5000</p>
                     </div>
-                    <div className="text-center">
+                    <div className="text-center hidden md:block">
                         <img src="https://eyeframeng.com/wp-content/uploads/2021/01/Mitch.jpg" />
                         <p>Eyeglasses Mitch</p>
                         <p className="text-orange-600 font-semibold py-2 text-xl">#15000</p>
                     </div>
-                    <div className="text-center">
+                    <div className="text-center hidden md:block">
                         <img src="https://eyeframeng.com/wp-content/uploads/2021/01/Zabel-C4.jpg" />
                         <p>Eyeglasses Zabel-C4</p>
                         <p className="text-orange-600 font-semibold py-2 text-xl">#10000</p>
                     </div>
-                    <div className="text-center">
+                    <div className="text-center hidden md:block">
                         <img src="https://eyeframeng.com/wp-content/uploads/2020/12/Eyeframeng-18.jpg" />
                         <p>Eyeglasses Eyeframeng-18</p>
                         <p className="text-orange-600 font-semibold py-2 text-xl">#20000</p>
                     </div>
                 </div>
-                <div className="flex flex-col md:flex-row md:mx-12 lg:mx-24">
+                <div className="flex  md:flex-row md:mx-12 lg:mx-24">
                     <div className="text-center">
                         <img src="https://eyeframeng.com/wp-content/uploads/2020/12/Theo-WD9511.jpg" />
                         <p>Eyeglasses Theo WD9511</p>
                         <p className="text-orange-600 font-semibold py-2 text-xl">#5000</p>
                     </div>
-                    <div className="text-center">
+                    <div className="text-center hidden md:block">
                         <img src="https://eyeframeng.com/wp-content/uploads/2021/01/Mitch.jpg" />
                         <p>Eyeglasses Mitch</p>
                         <p className="text-orange-600 font-semibold py-2 text-xl">#15000</p>
                     </div>
-                    <div className="text-center">
+                    <div className="text-center hidden md:block">
                         <img src="https://eyeframeng.com/wp-content/uploads/2021/01/Zabel-C4.jpg" />
                         <p>Eyeglasses Zabel-C4</p>
                         <p className="text-orange-600 font-semibold py-2 text-xl">#10000</p>
                     </div>
-                    <div className="text-center">
+                    <div className="text-center hidden md:block">
                         <img src="https://eyeframeng.com/wp-content/uploads/2020/12/Eyeframeng-18.jpg" />
                         <p>Eyeglasses Eyeframeng-18</p>
                         <p className="text-orange-600 font-semibold py-2 text-xl">#20000</p>
