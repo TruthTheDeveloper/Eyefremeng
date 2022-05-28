cd252d083597cb01e164070aea208be3e1003666
TruthTheDeveloper <henrytruthidongesit@gmail.com>
2022-05-28 00:07:41 -0700

    removed antd

--- src/containers/PrescriptionForm/components/LensForm.js
+++ src/containers/PrescriptionForm/components/LensForm.js
@@ -1,15 +1,14 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircle } from '@fortawesome/free-solid-svg-icons';//
 
-import { Collapse } from 'antd';
 import { CaretRightOutlined } from '@ant-design/icons';
-
+import { Collapse } from 'antd';
 const { Panel } = Collapse;
-
 const text = `
   A dog is a type of domesticated animal.
   Known for its loyalty and faithfulness,
-  it can be found as a welcome guest in many households across the world.`
+  it can be found as a welcome guest in many households across the world.
+`;
 
 const LensForm = () => {
 
@@ -23,24 +22,23 @@ const LensForm = () => {
                 <h1 className="text-white text-xs">All lenses includes Anti-Scratch and Anti-Reflective Coating</h1>
             </div>
             <Collapse
-                showArrow={false}
                 bordered={false}
                 defaultActiveKey={['1']}
-                // expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
-                className="site-collapse-custom-collapse"
+                expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
+                
             >
-                <Panel header="Clear" key="1" className="site-collapse-custom-panel">
+                <Panel header="This is panel header 1" key="1" >
                 <p>{text}</p>
                 </Panel>
-                <Panel header="PhotoChromic" key="2" className="site-collapse-custom-panel">
+                <Panel header="This is panel header 2" key="2" >
                 <p>{text}</p>
                 </Panel>
-                <Panel header="Polarized" key="3" className="site-collapse-custom-panel">
+                <Panel header="This is panel header 3" key="3" >
                 <p>{text}</p>
                 </Panel>
-            </Collapse>
+                </Collapse>
         </form>
-    )
+        )
 }
 
 export default LensForm;
