57c74c8aa63fd38d28c0dbe1df217ad767ea2c29
TruthTheDeveloper <henrytruthidongesit@gmail.com>
2022-05-30 15:13:23 -0700

    coding the static page

--- src/containers/AboutUs/ReturnExchange.js
+++ src/containers/AboutUs/ReturnExchange.js
@@ -1,7 +1,11 @@
 const ReturnExchange = () => {
 
     return(
-        <></>
+        <section className="my-">
+            <div className="bg-i">
+                <h1 className="text-orange-500">RETURN AND EXCHANGE</h1>
+            </div>
+        </section>
     )
 
 }
