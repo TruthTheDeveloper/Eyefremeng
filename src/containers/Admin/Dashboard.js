31bafa56da68ceb03a6f07ed8f953217c48701b5
TruthTheDeveloper <henrytruthidongesit@gmail.com>
2022-05-31 05:47:28 -0700

    coding the dashboard

--- src/containers/Admin/Dashboard.js
+++ src/containers/Admin/Dashboard.js
@@ -6,22 +6,30 @@ const Dashboard = () => {
     return(
         <section className="py-24">
             <h1>Dashboard</h1>
-            <div className="bg-white shadow-2xl xl:mx-32">
-                <section className="">
-                    <div className="w-36 md:w-40 lg:w-44 my-1 mx-4">
-                        <img src="https://eyeframeng.com/wp-content/uploads/2020/11/cropped-eyeframeng-logo-1-1536x460-1.png"/>
-                    </div>
-                    <div>
-                        <FontAwesomeIcon icon={faBagShopping}/>
+            <div className="bg-white shadow-lg rounded-lg xl:mx-24 flex">
+                <section className="bg-indigo-800 text-white basis-1/6">
+                    <div className="flex py-6 justify-center">
+                        <FontAwesomeIcon icon={faBagShopping} className="mx-4 text-2xl"/>
                         <h1>Orders</h1>
                     </div>
-                    <div>
-                        <FontAwesomeIcon icon={faGift}/>
+                    <div className="flex py-6 justify-center">
+                        <FontAwesomeIcon icon={faGift} className="mx-4 text-2xl"/>
                         <h1>Products</h1>
                     </div>
                 </section>
-                <section>
-                    
+                <section className="w-full">
+                    <div className=" flex justify-center">
+                        <div className="border-2 mx-4 w-72">
+                            <h1 className=" p-3 py-2">Total Orders</h1>
+                            <div className="flex"><h1 className="p-2">200</h1>
+                            <FontAwesomeIcon icon={faBagShopping} className="mx-4 text-3xl text-indigo-800"/></div>
+                        </div>
+                        <div className="border-2 mx-4 ">
+                           <h1 className=" py-2">Total Delveries</h1>
+                           <h1>100</h1>
+                           <FontAwesomeIcon icon={faBagShopping} className="mx-4 text-3xl text-indigo-800"/>
+                        </div>
+                    </div>
                 </section>
             </div>
         </section>
