e7bbf33706af594b2f3f1c048ed4da7382a67118
TruthTheDeveloper <henrytruthidongesit@gmail.com>
2022-06-18 09:38:29 -0700

    implemented authentication

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -1,5 +1,8 @@
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
 
@@ -9,32 +12,68 @@ const Login = () => {
     const [password, setPassword] = useState('')
     const [passwordValidationError, setPaswordValidationError] = useState('')
 
-    const auth = getAuth();
+
+    const navigate = useNavigate()
     
+    
+    const auth = getAuth();
 
     const navigateToDashboard = () => {
 
         console.log(email, password, 'email password')
-
-        createUserWithEmailAndPassword(auth, email, password)
+        
+        signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             // Signed in 
             const user = userCredential.user;
-
             console.log(user)
+
+            navigate('/')
+            toast.success('🦄 Congratulations you succesfully place an order!', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                });
             // ...
         })
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
-            // ..
+            toast.error('🦄 unable to place order please contact support or try again', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                });
         });
+
+        
 
     }
 
 
     return(
         <div className="py-24 mx-6 xl:mx-64">
+            <ToastContainer
+                    position="top-right"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    />
+                    {/* Same as */}
+                    <ToastContainer />
             <h1 className="text-3xl text-slate-700 mb-8">Login</h1>
             <h1>if you have an account with us, please log in</h1>
             <div className="my-4 my-2">
--- src/containers/Auth/Register.js
+++ src/containers/Auth/Register.js
@@ -1,5 +1,9 @@
 import {useNavigate} from 'react-router-dom';
 import { useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { db } from "../../firebase/firebase-config";
+import { doc, setDoc } from "firebase/firestore"; 
+
 
 const Register = () => {
 
@@ -20,9 +24,43 @@ const Register = () => {
     const [confirmPassword, setConfirmPassword] = useState('')
     const [confirmPasswordValidationError, setConfirmPasswordValidationError] = useState('')
 
+    const auth = getAuth();
+
 
     const navigateToLogin = () => {
-        navigate('/login')
+        // navigate('/login')
+
+
+        
+    }
+
+    const registerHandler = () => {
+
+        console.log(email, password, firstName, lastName, 'lastname')
+
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            console.log(user.uid)
+           
+            setDoc(doc(db, 'users', user.uid), {
+                firstName:firstName,
+                lastName:lastName
+            })
+
+            console.log(user)
+            // ...
+        })
+        .then((res) => {
+            console.log(res)
+            console.log('successs')
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+        });
     }
 
     return(
@@ -33,35 +71,35 @@ const Register = () => {
                 </div>
                 <div className="my-4 my-2">
                     <label>Email Address</label><br/>
-                    <input className="w-full border-2 p-2 outline-none my-2" onChange={() => setEmail()}/>
+                    <input className="w-full border-2 p-2 outline-none my-2" onChange={(e) => setEmail(e.target.value)}/>
                     {emailValidationError && <p className="text-red-500 text-sm">Please input email field</p>}
                 </div>
                 <div className="md:flex my-2">
                     <div className="w-full md:mr-2">
                         <label>First Name</label><br/>
-                        <input className="w-full border-2  p-2 text-slate-700 outline-none my-2" onChange={() => setFirstName()}/>
+                        <input className="w-full border-2  p-2 text-slate-700 outline-none my-2" onChange={(e) => setFirstName(e.target.value)}/>
                         {firstNameValidationError && <p className="text-red-500 text-sm">Please input firstName field</p>}
                     </div>
                     <div className="w-full md:ml-2">
                         <label>Last Name</label><br/>
-                        <input className="w-full border-2  p-2 text-slate-700 outline-none my-2" onChange={() => setLastName()}/>
+                        <input className="w-full border-2  p-2 text-slate-700 outline-none my-2" onChange={(e) => setLastName(e.target.value)}/>
                         {lastNameValidationError && <p className="text-red-500 text-sm">Please input lastName field</p>}
                     </div>
                 </div>
                 <div className="md:flex my-2">
                     <div className="w-full md:mr-2">
                         <label>Password</label><br/>
-                        <input className="w-full border-2  p-2 text-slate-700 outline-none my-2" onChange={() => setPassword()}/>
+                        <input className="w-full border-2  p-2 text-slate-700 outline-none my-2" onChange={(e) => setPassword(e.target.value)}/>
                         {passwordValidationError && <p className="text-red-500 text-sm">Please input password field</p>}
                     </div>
                     <div className="w-full md:ml-2">
                         <label>Confirm Password</label><br/>
-                        <input className="w-full border-2  p-2 text-slate-700 outline-none my-2" onChange={() => setConfirmPassword()}/>
+                        <input className="w-full border-2  p-2 text-slate-700 outline-none my-2" onChange={(e) => setConfirmPassword(e.target.value)}/>
                         {confirmPasswordValidationError && <p className="text-red-500 text-sm">Please input confirm Password field</p>}
                     </div>
                 </div>
                 <div className="my-4">
-                    <button className="py-3 bg-indigo-800 text-white py-2 px-6 rounded-md">Submit</button>
+                    <button className="py-3 bg-indigo-800 text-white py-2 px-6 rounded-md" onClick={registerHandler}>Submit</button>
                     <button className="py-3 py-2 px-6 mx-3 border rounded-md border-indigo-800 text-indigo-800" onClick={navigateToLogin}>Login</button>
                 </div>
             </div>
