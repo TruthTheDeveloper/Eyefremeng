0b382deaf34519852faf933b823b0cb196e49200
olawalecodes <iwaloyeo@gmail.com>
2023-01-13 17:12:14 +0100

    updates: changed footer sunglasses name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -36,7 +36,7 @@ const Footer = () => {
             </li>
             <li>
               <NavLink to="/" exact="true">
-                SunGlasses
+                Sunglasses
               </NavLink>
             </li>
           </ul>
